c0d65e8a40799ab22f21842a3033a8da0ed98f95
kimjaebeom98 <87630540+kimjaebeom98@users.noreply.github.com>
2022-06-21 17:00:15 +0900

    [Silver I] Title: 음식물 피하기, Time: 296 ms, Memory: 32468 KB -BaekjoonHub

---
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -0,0 +1,45 @@
+from collections import deque
+n, m, k = map(int, input().split())
+
+board = list([0 for _ in range(m)] for _ in range(n))
+
+# 쓰레기 위치에 1로 표시
+for _ in range(k):
+    r, c = map(int, input().split())
+    board[r-1][c-1] = 1
+
+# 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y, board):
+    queue = deque()
+    queue.append((x,y))
+    result = 1
+    board[x][y] = 2
+
+    
+    while queue:
+        a, b = queue.popleft()
+        
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
+                queue.append((nx, ny))
+                board[nx][ny] = 2
+                result += 1
+    return result
+            
+answer = 0
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 1:
+            ans = bfs(i, j, board)
+            answer = max(ans, answer)
+
+print(answer)
+
+
