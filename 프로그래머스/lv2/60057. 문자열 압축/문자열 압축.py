39f4c477dbd49cb4b4b0a20ff6e1b4b08c2db69d
kimjaebeom98 <87630540+kimjaebeom98@users.noreply.github.com>
2022-07-12 15:02:29 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,22 @@
+def solution(s):
+  c = 1
+  count = 1
+  iter = s[:c]
+  min_len = len(s)
+  for i in range(1, len(s)//2+2):
+    new_s = ''
+    iter = s[:i]
+    count = 1
+    for j in range(i, len(s)+i, i):
+      if s[j:j+i] == iter:
+        count+=1
+      else:
+        if count != 1:
+          new_s += str(count)
+        
+        new_s += iter
+        iter = s[j:j+i]
+        count = 1
+    
+    min_len = min(min_len, len(new_s))    
+  return min_len
