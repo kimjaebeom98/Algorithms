93c5dca3f8a85d3b334e05760e78460ea3706ce3
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-09-13 01:31:16 +0900

    [level 2] Title: 오픈채팅방, Time: 75.69 ms, Memory: 46.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,23 +1,30 @@
+"""
+dictionary는 항상 마지막값을 저장함
+Enter일 때는 result에 [uid, "님이 들어왔습니다."] 저장
+Leave일 때는 result에 [uid, "님이 나갔습니다."] 저장
+Change일 때는 chk값 갱신
+
+"""
+
 def solution(record):
-  chage = {}
-
-  for info in record:
-    temp = info.split(' ')
-    if temp[0] != 'Leave':
-      chage[temp[1]] = temp[2]
-
-  answer = []
-
-  for info in record:
-    temp = info.split(' ')
-    temp_s = ''
-    name = chage[temp[1]]
-    if temp[0] == 'Enter':
-      temp_s = name + "님이 들어왔습니다."
-    elif temp[0] == 'Leave':
-      temp_s = name + "님이 나갔습니다."
-    else:
-      continue
-
-    answer.append(temp_s)
-  return answer
+    chk = {}
+    result = []
+    for re in record:
+        tmp = re.split(' ')
+        if len(tmp) == 2:
+            in_out, uid = tmp[0], tmp[1]
+        else:
+            in_out, uid, name = tmp[0], tmp[1], tmp[2]
+        if in_out == 'Enter':
+            result.append([uid, "님이 들어왔습니다."])
+        elif in_out == 'Leave':
+            result.append([uid, "님이 나갔습니다."])
+            continue
+        chk[uid] = name
+    
+    for i, data in enumerate(result):
+        last = chk[data[0]]
+        result[i] = last + data[1]
+    
+    return result
+            
