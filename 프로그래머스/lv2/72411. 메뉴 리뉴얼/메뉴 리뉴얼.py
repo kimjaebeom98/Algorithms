9fe9d04e0e28eb313fd570d098fbc53e40fd43b4
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-09-14 19:00:09 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,25 +1,37 @@
+"""
+course로 첫 for문을 시작했을 때, 
+예를 들어 c가 2라고 하면 orders를 뽑아내서 combination 적용시켜서
+하면됨 그러고나서 Counter
+"""
 from itertools import combinations
 from collections import Counter
 
-
 def solution(orders, course):
-
-    answer = []
+    result = []
+    chk = {}
     for c in course:
         tmp = []
         for order in orders:
-            for comb in combinations(order, c):
-                tmp_s = ''.join(sorted(comb))
-                tmp.append(tmp_s)
-
-        tmp = Counter(tmp).most_common()
-        if len(tmp) >= 1:
-            _max = tmp[0][1]
-        else:
-            continue
-        for i in tmp:
-            if i[1] == _max and i[1] >= 2:
-                answer.append(i[0])
+            order = list(order)
+            order.sort()
+            # course요리를 구성하는 메뉴 갯수 만큼의 조합을 뽑아냄
+            for combi in combinations(order, c):
+                combi = ''.join(combi)
+                tmp.append(combi)
+        new = Counter(tmp).most_common()
   
-    return sorted(answer)
-    
+        if len(new) == 0 or new[0][1] < 2:
+            continue
+        result.append(new[0][0])
+        _max = new[0][1]
+        for data in new[1:]:
+            if data[1] < _max:
+                break
+            else:
+                result.append(data[0])
+       
+    result.sort()
+    return result
+        
+        
+        
