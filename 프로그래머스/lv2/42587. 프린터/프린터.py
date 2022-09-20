3a22a0efb6f64a417a71d891e7e453402f075e73
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-09-21 01:43:45 +0900

    [level 2] Title: 프린터, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,31 +1,28 @@
+"""
+내가 요청한 문서가 몇 번째로 인쇄되는지
+1. 내가 요청한 문서가 무엇인지 알기 위해서는 초반에 [우선순위, index]로 새로 배열을 만듬
+2. 아직 빠져나가지 못한 것 중 젤 큰 우선순위 추적
+3. 한 개를 뽑고, 만약 이것보다 큰 우선순위가 있으면 맨뒤로, 아니면
+지금까지 젤 큰 우선순위라는 소리니깐 내가 찾는것인지 비교
+"""
 from collections import deque
 
 def solution(priorities, location):
-    priority = deque()
-    for idx, data in enumerate(priorities):
-      priority.append([idx, data])
-
-    cnt = 0
-    i = 0
-    while True:
-
-      flag = 0
-      data = priority[0][1]
-
-      for j in priority:
-        if j[1] > data:
-          flag = 1
-
-
-      if flag == 1:
-        tmp = priority.popleft()
-        priority.append(tmp)
-      else:
-        cnt+=1
-        if priority[0][0] == location:
-          break
-        priority.popleft()
-
-      i += 1
+    cnt = 1
+    _max = deque(sorted(priorities, reverse=True))
+    new = deque([[data, idx] for idx, data in enumerate(priorities)])
     
-    return cnt
+    while True:
+        pri, idx = new.popleft()
+        if _max[0] > pri:
+            new.append([pri, idx])
+        else:
+            if idx == location:
+                return cnt
+            else:
+                _max.popleft()
+                cnt += 1
+    
+            
+                
+                
