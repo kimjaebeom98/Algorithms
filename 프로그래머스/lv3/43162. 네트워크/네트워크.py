ade40c463603694d5fd46163c22dee1a5a3a792e
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-09-22 18:49:47 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,25 +1,26 @@
 def solution(n, computers):
-    graph = [[] for _ in range(n)]
-
-    for i, d in enumerate(computers):
-      for idx, data in enumerate(d):
-        if idx == i:
-          continue
-        if data == 1:
-          graph[i].append(idx)
-
-    visited = [-1] * n
-    def dfs(start):
-      nonlocal visited, graph  
-      visited[start] = 1
-
-      for i in graph[start]:
-        if visited[i] == -1:
-          dfs(i)
-
+    chk = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if computers[i][j]:
+                chk[i].append(j)
+    visited = [0] * n
+    def dfs(idx):
+        nonlocal visited, chk
+        visited[idx] = 1
+        
+        for i in chk[idx]:
+            if not visited[i]:
+                dfs(i)
     count = 0
     for i in range(n):
-      if visited[i] == -1:
-        dfs(i)
-        count+=1
+        if not visited[i]:
+            dfs(i)
+            count += 1
+    
     return count
+        
+                
+    
