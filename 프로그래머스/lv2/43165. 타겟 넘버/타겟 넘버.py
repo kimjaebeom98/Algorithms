c24ffe7db3494dc4d3777d940452c04e6d4a9560
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-09-22 16:53:57 +0900

    [level 2] Title: 타겟 넘버, Time: 2.99 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,18 @@
+"""
+dfs로 풀것
+dfs에 인자로 idx와 _sum을 보냄
+만약 idx가 len(numbers)와 같으면 그냥 return
+만약 _sum이 target값과 같으면 res+=1
+"""
 def solution(numbers, target):
-  n = len(numbers)
-  answer = 0
-  def dfs(idx, result):
-    nonlocal answer
-    if idx == n: 
-      if result == target:
-        answer += 1
-      return
-    dfs(idx+1, result - numbers[idx])
-    dfs(idx+1, result + numbers[idx])
-  
-  dfs(0, 0)
-  return answer
+    def dfs(idx, _sum):
+        nonlocal numbers, target
+        if idx+1 == len(numbers):
+            if _sum == target:
+                return 1
+            return 0
+        
+        return dfs(idx+1, _sum + numbers[idx+1]) + dfs(idx+1, _sum - numbers[idx+1])
+    
+    return dfs(0, numbers[0]) + dfs(0, numbers[0]*(-1))
+    
