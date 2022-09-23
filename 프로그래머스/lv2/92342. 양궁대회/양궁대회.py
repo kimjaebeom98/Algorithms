49a5d6b383942f91d7a0a1e51e82ada150a0ab61
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-09-23 22:38:31 +0900

    [level 2] Title: 양궁대회, Time: 498.08 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,55 +1,51 @@
 """
-1. k(1~10) 점을 각각 a발, b발 맞췄으면 a>b일때 a가 k점을 가져감,
-만약 a == b 이면 어피치가 k점을 가져감 그러나 a == b == 0이면 아무도  안가져감
+화살의 개수를 담은 자연수 n
+어피치 과녁 점수의 개수를 10점부터 0점까지 순서대로 담은 정수 배열 info
+k점을 라이언이 더 많이 맞히면 k점을 라이언이 가져감
+k점을 똑같이 맞추면 어피치가 가져감
+k점을 0발이면 둘 다 0점
+라이언이 가장 큰 점수차이로 우승하기 위해 n발의 화살을 어떤 과녁 점수에 맞혀야 할지 배열로담아 리턴
+우승할 수 없으면 -1
 
-2. 최종점수가 더 높으면 우승 그러나 최종점수가 같으면 어피치 우승
-
-어피치가 n발을 맞춘 과녁의 점수들(info)이 주어질 때, 라이언이 가장 큰 점수 차이로 우승하기 우히ㅐ n발을 어떤 과녁
-점수에 맞춰야 할지 return
-
-info의 i번째 원소는 10-i점을 맞힌 화살 개수(return도 이런식으로 해야함)
-라이언이 가장 큰 점수차이로 우승할 수 있는 방법이 여러개이면 가장 낮은 점수를 더많이 맞힌 경우를 return
+info의 i번째 원소는 과녁의 10-i점을 맞힌 화살 개수
+n발을 꼭 다써야함
+우승할 방법이 여러가지일 경우 가장 낮은 점수를 더 많이 맞힌 경우를 return
+가장 낮은 점수를 맞힌 개수가 같을 경우 계속해서 그다음으로 낮은 점수를 더 많이 맞힌 경우를 return
 """
-
-"""
-1. (0~ 10)사이의 점수 중 n개의 화살로 맞힐 수 있는 점수의 중복조합을 뽑아냄
-2. 라이언 점수판을 만들어냄
-3. 비교하면서 라이언 점수를 구하고, 이 점수가 기존에 max값보다 크면 다 비워냄,
-같으면 붙임 
-"""       
 from itertools import combinations_with_replacement
 from collections import deque
 
 def solution(n, info):
-    comb = deque()
-    for i in combinations_with_replacement(range(11), n):
-        comb.append(i)
-    
-    # [10, 10, 3, 4, 1]
+    comb = []
+    for c in combinations_with_replacement(range(11), n):
+        comb.append(c)
+        
     _max = 0
     res = [-1]
-    while comb :
-        lion = [0] * 11
-        tmp = comb.popleft()
-        for c in tmp:
-            lion[10-c] += 1
-        
+    for c in comb:
+        result = [0] * 11
+        for score in c:
+            result[10-score] += 1
+            
         ap = 0
         li = 0
-        for l in range(len(lion)):
-            if not (info[l] == 0 and lion[l] == 0):
-                if info[l] >= lion[l]:
+        for l in range(len(result)):
+            if not (info[l] == 0 and result[l] == 0):
+                if info[l] >= result[l]:
                     ap += 10 - l
                 else:
                     li += 10 - l
         ch = li - ap
-        if ch > _max :
+        if ch > _max:
             _max = ch
-            res = lion
+            res = result
+    
     
     return res
+    
             
-                
+    
+    
                 
         
     
