07a29ab712f3a19897f936621edd665b9efeb504
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-10-28 15:27:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1320 ms, Memory: 105984 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,26 +2,23 @@ import sys
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-board = [list(map(int, input().split())) for _ in range(n)]
-
-
-# s[x][y] = x행 y열 까지의 합
 s = [[0 for _ in range(n+1)] for _ in range(n+1)]
-s[1][1] = board[0][0]
 
-for i in range(1, n+1):
-  for j in range(1,n+1):
-    if i == 0 and j == 0:
-      continue
-    else:
-      s[i][j] = s[i][j-1] + board[i-1][j-1]
+s[1][1] = arr[0][0]
+# 1행
+for i in range(2, n+1):
+    s[1][i] = s[1][i-1] + arr[0][i-1]
+# 1열
+for i in range(2, n+1):
+    s[i][1] = s[i-1][1] + arr[i-1][0]
 
-  if i != 1 :
-    for j in range(1, n+1):
-      s[i][j] = s[i-1][j] + s[i][j]
+for i in range(2, n+1):
+    for j in range(2, n+1):
+        s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + arr[i-1][j-1]
 
 for _ in range(m):
-  x1, y1, x2, y2 = map(int, input().split())
-  print(s[x2][y2] - s[x1-1][y2] - s[x2][y1-1] + s[x1-1][y1-1])
-  
+    x1, y1, x2, y2 = map(int, input().split())
+    res = s[x2][y2] - s[x2][y1-1] - s[x1-1][y2] + s[x1-1][y1-1]
+    print(res)
