645ddf1ad60e9732f6ae278fc66082babb706203
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-10-28 19:46:31 +0900

    [Silver V] Title: 숫자 카드, Time: 1276 ms, Memory: 113476 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,29 +1,16 @@
-
-
+from bisect import bisect_left, bisect_right
 
 n = int(input())
-cards = list(map(int, input().split()))
-cards.sort()
+having = list(map(int, input().split()))
 m = int(input())
-target_arr = list(map(int, input().split()))
+chk = list(map(int, input().split()))
 
+having.sort()
 
-def binary_search(num):
-    l_idx = 0
-    r_idx = n-1
-
-    while l_idx <= r_idx:
-        mid_idx = (l_idx + r_idx) // 2
-        if cards[mid_idx] == num:
-            return 1
-
-        elif cards[mid_idx] < num:
-            l_idx = mid_idx + 1
-        else:
-            r_idx = mid_idx - 1
-    return 0
-
-
-for target in target_arr:
-    res = binary_search(target)
-    print(res, end=' ')
+for target in chk:
+    left_idx = bisect_left(having, target)
+    right_idx = bisect_right(having, target)
+    if right_idx - left_idx == 1:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
