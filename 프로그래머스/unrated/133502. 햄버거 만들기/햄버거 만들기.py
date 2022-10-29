a6dfcff7c93aaee499dceb04f024007b82e465d6
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2022-10-29 17:16:44 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -6,13 +6,10 @@ def solution(ingredient):
     answer = 0
     stk = []
     for i in ingredient:
-        if len(stk) < 3:
-            stk.append(i)
-        else:
-            stk.append(i)
-            if stk[-1:-5:-1] == [1, 3, 2, 1]:
-                answer += 1
-                for _ in range(4):
-                    stk.pop()
+        stk.append(i)
+        if stk[-4:] == [1, 2, 3, 1]:
+            answer += 1
+            for _ in range(4):
+                stk.pop()
                 
     return answer
