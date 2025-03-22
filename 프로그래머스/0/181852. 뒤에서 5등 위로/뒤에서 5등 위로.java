3461c97921758d813ce5518e11b17011c8ef389b
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2025-03-22 14:47:59 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 1.18 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -2,15 +2,11 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> list = new ArrayList<>();
-        for(int num : num_list){
-            list.add(num);
-        }
-        Collections.sort(list);
+        Arrays.sort(num_list);
         
         int[] answer = new int[num_list.length-5];
         for(int i=0; i<num_list.length-5; i++)
-            answer[i] = list.get(i+5);
+            answer[i] = num_list[i+5];
         return answer;
     }
 }
