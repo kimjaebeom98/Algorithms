ff8da71f5cac3638678d6eaea34c51cadb0258e3
Jae Beom Kim <87630540+kimjaebeom98@users.noreply.github.com>
2025-09-11 21:37:11 +0900

    Update 가장 큰 수.java

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -6,7 +6,13 @@ class Solution {
         List<String> resList = new ArrayList<>();
         for(int number : numbers)
             resList.add(Integer.toString(number));
-        
+        // (s2 + s1).compareTo(s1 + s2) = (s2 + s1) - (s1 + s2)
+        // s1 = "3", s2 = "30" 이라고 가정했을 때
+        // "303".compareTo("330") = "303" - "330"(아스키코드값) = (-) 
+        // compare(A, B)의 결과가 음수(-)면 A가 B보다 작다고 생각하고 A를 B앞에 배치(기본 오름차순이니깐)
+        // 따라서 "3" 그다음 "30"이 옴 
+        // s1 = "30", s2 = "3"이면 양수(+)가 나올거고 A가 B보다 크다고 생각하고 A를 B뒤에 배치
+        // Collections.sort가 정렬을 할 때, 리스트 안의 모든 요소들을 가져다가 '둘씩 짝 지어서' 어떤 게 앞에 와야 할지 계속 물어보고 그에 따라 순서를 바꿈
         Collections.sort(resList, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
         
         if("0".equals(resList.get(0)))
